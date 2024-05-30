3702b8be7ac68af30acda792dbdc3e499428f444
soaryong <soaryong.c@gmail.com>
2024-05-30 09:00:04 +0900

    dev db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,24 @@ import { UserCoin } from './user_coin/user_coin.entity';
 import { TradeModule } from './trade/trade.module';
 import { Trade } from './trade/trade.entity';
 
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'hackathemy.me',
+//       port: 3306,
+//       username: 'adfpump',
+//       password: 'Adfadf!1',
+//       database: 'adfpump',
+//       synchronize: true,
+//       entities: [Users, Coins, CoinStat, UserCoin, Trade],
+//     }),
+//     UserModule,
+//     CoinModule,
+//     TradeModule,
+//   ]
+// })
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
